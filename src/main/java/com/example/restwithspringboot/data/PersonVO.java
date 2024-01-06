b0c624196ad3df41135fb9989929e1b870b3270e
sergioperess <sergio_kaun@hotmail.com>
2024-01-06 13:44:22 -0300

    Json commit

--- src/main/java/com/example/restwithspringboot/data/PersonVO.java
+++ src/main/java/com/example/restwithspringboot/data/PersonVO.java
@@ -2,14 +2,25 @@ package com.example.restwithspringboot.data;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id","first_name","last_name","adress","gender"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
     private String adress;
+    
+    @JsonIgnore
     private String gender;
 
     public PersonVO(){
